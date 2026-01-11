f37736842aa4dcb28851db6731b1f7846c35e6b4
Apurv Chatur <official.apurv.chatur@gmail.com>
2026-01-11 20:21:16 +0530

    XXX-004: updated brand connection for all three projects (backend, admin, frontend)

--- site/backend/src/aConnection/jBrandConnection.ts
+++ site/backend/src/aConnection/jBrandConnection.ts
@@ -1,16 +1,16 @@
 const brandConnection = {
-  aBrandID: "boilerplate",
-  bBrandName: "Boilerplate",
+  aBrandID: "ankas",
+  bBrandName: "Ankas",
 
-  cFrontendApplicationName: "Boilerplate Frontend",
-  dAdministrationApplicationName: "Boilerplate Administration",
-  eBackendApplicationName: "Boilerplate Backend",
+  cFrontendApplicationName: "Ankas Frontend",
+  dAdministrationApplicationName: "Ankas Administration",
+  eBackendApplicationName: "Ankas Backend",
 
-  fFrontendApplicationURL: "https://boilerplate-frontend-v02.netlify.app",
-  gAdminApplicationURL: "https://boilerplate-admin-v02.netlify.app",
-  hBackendApplicationURL: "https://boilerplate-backend-v02.netlify.app",
+  fFrontendApplicationURL: "https://ankas-frontend-v02.netlify.app",
+  gAdminApplicationURL: "https://ankas-admin-v02.netlify.app",
+  hBackendApplicationURL: "https://ankas-backend-v02.netlify.app",
 
-  iEmailName: "@boilerplate.com",
+  iEmailName: "@ankas.com",
 
   jInstagramURL: "https://www.google.com/",
   kXURL: "https://www.google.com/",
@@ -24,7 +24,7 @@ const brandConnection = {
       : this.pBackendLocalURL;
   },
   pBackendLocalURL: "http://localhost:8000",
-  qBackendProductionURL: "https://boilerplate-backend-v02.onrender.com",
+  qBackendProductionURL: "https://ankas-backend-v02.onrender.com",
 
   get rFrontendBaseURL() {
     return process.env.ENVIRONMENT === "Production" ? [ 
@@ -51,10 +51,10 @@ const brandConnection = {
   sFrontendLocalURL7: "http://localhost:5179",
   sFrontendLocalURL8: "http://localhost:5180",
   sFrontendLocalURL9: "http://localhost:5181",
-  tFrontendProductionURL1: "https://boilerplate-admin-v02.netlify.app",
-  tFrontendProductionURL2: "https://boilerplate-frontend-v02.netlify.app",
+  tFrontendProductionURL1: "https://ankas-admin-v02.netlify.app",
+  tFrontendProductionURL2: "https://ankas-frontend-v02.netlify.app",
 
-  uTagName: "Skip setup. Start building.",
+  uTagName: "Fueling Applications With Strong Backend",
 }
 
 export default brandConnection;
